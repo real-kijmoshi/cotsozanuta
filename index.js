494d015b5c11d6e29efc35bb97e16b80349accb1
kijmoshi <94071547+real-kijmoshi@users.noreply.github.com>
2026-05-31 12:41:26 +0200

    design improvments

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const genius = require("./utils/genius");
 const db = require("./utils/db");
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 3000;
 
 app.use(helmet({
